246546e9a76e8d697b27800fd4f582e44f0a755d
Raja Telagatoti <rajatelagathoti19@gmail.com>
2026-07-01 21:09:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,6 @@ import streamlit as st
 from gtts import gTTS
 import io
 text = "Namasthe, Mera naam Raja, Log mujhe kehte hai Rockey Bhai"
-if st.button("Play Telugu Speech"):
-    tts = gTTS(text=text, lang="te", slow=False)
-
-    audio = io.BytesIO()
-    tts.write_to_fp(audio)
-    audio.seek(0)
-
-    st.audio(audio, format="audio/mp3")
 st.markdown("""
 <!DOCTYPE html>
 <html>
@@ -23,7 +15,14 @@ st.markdown("""
   <h6>Click the button below to download Qur'an</h6>
   <a href = "https://github.com/rajatelagathoti/Hapsa/blob/main/Telugu%20Quran.pdf">Click here</a>
   <h6>Click the button below to listen to the Qur'an</h6>
-  <a href = "https://github.com/rajatelagathoti/blank-app/blob/main/1-%20Mr.%20Perfect-SenSongsMp3.Co.mp3">Click here</a>
-</body>
+  </body>
 </html>
 """, unsafe_allow_html=True)
+if st.button("Play Telugu Speech"):
+    tts = gTTS(text=text, lang="te", slow=False)
+
+    audio = io.BytesIO()
+    tts.write_to_fp(audio)
+    audio.seek(0)
+
+    st.audio(audio, format="audio/mp3")
