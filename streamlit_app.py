537fd4c51bcb8acc2c6cc7b35917d1e9846005c6
Raja Telagatoti <rajatelagathoti19@gmail.com>
2026-07-02 10:29:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ st.markdown("""
     Raja Telagathoti
   </h1>
   <h6>Click the button below to download Qur'an</h6>
-  <a href = "https://github.com/rajatelagathoti/Hapsa/blob/main/Telugu%20Quran.pdf">Click here</a>
+  <a href="https://raw.githubusercontent.com/rajatelagathoti/Hapsa/main/Telugu%20Quran.pdf" target="_blank">
+Click here to download Telugu Qur'an
+</a>
   <h6>Click the button below to listen to the Qur'an</h6>
   </body>
 </html>
